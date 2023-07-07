920f9ca53e7cc131d2482eba1455ab82615a1af3
Gulzaib Ahmad <zaibahmad789@gmail.com>
2023-07-07 12:11:03 +0500

    complete

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import './contact.css'
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can add your logic to handle the form submission, such as sending an email or saving the data to a database
+    // Once the submission is successful, you can set the "submitted" state to true
+    setSubmitted(true);
+  };
+
   return (
     <section id="contact">
       <br />
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h5>Get In Touch</h5>
+      <h2>Contact Form</h2>
+
+      {submitted ? (
+        <div>
+          <p>Thank you for reaching out! We will get back to you soon.</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </section>
-  )
+  );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import { FaFacebook, FaTwitter } from 'react-icons/fa';
+import { AiFillInstagram } from 'react-icons/ai';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div className="social-media">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook className='icon'/>
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter className='icon'/>
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <AiFillInstagram className='icon'/>
+        </a>
+      </div>
+      <div className="footer">
+        <p className="foot">&copy; {new Date().getFullYear()} Dave Marcel. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
